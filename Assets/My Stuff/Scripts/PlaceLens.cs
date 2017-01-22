f9e75c513012ca5eddd1bf336959f6a287449cdb
Bo Brinkman <dr.bo.brinkman@gmail.com>
2017-01-22 00:15:28 -0500

    Improved bowl placement

--- Assets/My Stuff/Scripts/PlaceLens.cs
+++ Assets/My Stuff/Scripts/PlaceLens.cs
@@ -14,10 +14,37 @@ public class PlaceLens : MonoBehaviour {
     void Update () {
         var headPosition = mainCam.transform.position;
         var gazeDirection = mainCam.transform.forward;
-        //Put bowl 1 meter down (y) from head, in direction of gaze with y set to 0.
-        gazeDirection.y = 0;
+        //Put bowl .5 meter down (y) from head, in direction of gaze with y set to 0.
+        /*gazeDirection.y = 0;
         gazeDirection.Normalize();
         headPosition.y -= 0.5f; //Move down .5 meters from head
-        gameObject.transform.localPosition = headPosition + gazeDirection*0.25f; //Assuming gazeDirection is a unit vector, so 1 meter
+        gameObject.transform.localPosition = headPosition + gazeDirection*0.25f; //Assuming gazeDirection is a unit vector, so 1 meter*/
+
+        //Actually: Bowl should be .5 meters down from head, and in line with the person's gaze. It cannot be farther away than .5 meters,
+        // which is the average length of a forearm
+        var bowlPos = headPosition;
+        if (gazeDirection.y < -0.01f)
+        {
+            var amountOfGazeDirection = -0.5f / gazeDirection.y;
+            bowlPos = headPosition + amountOfGazeDirection * gazeDirection;
+            //TODO: bring bowl back if too far away
+            headPosition.y -= 0.5f;
+            var bowlDist = Vector3.Distance(headPosition, bowlPos);
+            if(bowlDist > 0.5f)
+            {
+                //Bowl too far, bring it back
+                var bowlDir = bowlPos - headPosition;
+                bowlDir.Normalize();
+                bowlPos = headPosition + bowlDir * 0.5f;
+            }
+        } else
+        {
+            gazeDirection.y = 0;
+            gazeDirection.Normalize();
+            bowlPos = headPosition + gazeDirection;
+            bowlPos.y -= 0.5f;
+        }
+
+        gameObject.transform.position = bowlPos;
 	}
 }
