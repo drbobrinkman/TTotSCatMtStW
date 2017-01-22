8b4feebd92ef27831411bb8be5086cedbb99dd34
Bo Brinkman <dr.bo.brinkman@gmail.com>
2017-01-21 21:48:13 -0500

    Basic walk and idle

---
+++ Assets/My Stuff/Scripts/KeepAvatarOnFloor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepAvatarOnFloor : MonoBehaviour {
+
+    private GameObject mainCam;
+
+    private void Start()
+    {
+        mainCam = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var headPosition = mainCam.transform.position;
+        var curPosition = gameObject.transform.position;
+        curPosition.y = headPosition.y - 1.5f; //estimating that eyes to feet distance is 1.5m 
+        gameObject.transform.position = curPosition; //Assuming gazeDirection is a unit vector, so 1 meter
+    }
+}
---
+++ Assets/My Stuff/Scripts/WalkToTarget.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkToTarget : MonoBehaviour {
+
+    private GameObject walkTarget;
+    private GameObject mainCam;
+    private Animator anim;
+
+    private void Start()
+    {
+        walkTarget = GameObject.Find("Walk Target");
+        mainCam = GameObject.Find("Main Camera");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cutOff = 0.1f; //10cm
+
+        var footPosition = walkTarget.transform.position;
+        var headPosition = mainCam.transform.position;
+        var avPosition = gameObject.transform.position;
+        //Ignore elevation differences
+        headPosition.y = 0.0f;
+        avPosition.y = 0.0f;
+
+        var dist = Vector3.Distance(headPosition, avPosition);
+        if(dist > cutOff)
+        {
+            anim.SetFloat("Walk", 1.0f);
+            gameObject.transform.LookAt(walkTarget.transform);
+
+        } else
+        {
+            anim.SetFloat("Walk", 0.0f);
+        }
+    }
+}
