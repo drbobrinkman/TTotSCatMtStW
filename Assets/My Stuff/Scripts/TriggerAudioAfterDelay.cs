0bf92c125e490b1961d0c30b36b4bc31feda66a2
Bo Brinkman <dr.bo.brinkman@gmail.com>
2017-01-22 12:47:48 -0500

    Story added, plays after a 10s delay

---
+++ Assets/My Stuff/Scripts/TriggerAudioAfterDelay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudioAfterDelay : MonoBehaviour {
+    private AudioSource storySource;
+    public float delayInSeconds = 10.0f;
+    float startTime;
+    bool done = false;
+    // Use this for initialization
+    void Start () {
+        storySource = GetComponent<AudioSource>();
+        startTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(!done && Time.time - startTime > delayInSeconds)
+        {
+            done = true;
+            storySource.Play();
+        }
+	}
+}
